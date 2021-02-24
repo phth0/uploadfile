b8148273c760a75b4a7cc9efac3c93e6bc38f4ae
chenxinhu <xingshi8000@gmail.com>
2021-02-24 14:51:52 +0800

    fix

--- src/reqlog.middleware.ts
+++ src/reqlog.middleware.ts
@@ -1,8 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { InterfaceType } from '@nestjs/graphql';
 
 import { Request, Response, NextFunction } from 'express';
-import { logger } from '../services/log.service';
 
 let reqN = 0;
 @Injectable()
@@ -11,16 +9,14 @@ export class ReqlogMiddleware implements NestMiddleware {
     const { ip, method, path: url, baseUrl } = request;
     const userAgent = request.get('user-agent') || '';
     const inT = Date.now();
-    logger().setContext(`${reqN++}`);
     response.on('close', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
       const data = response.get('body');
       const t = Date.now() - inT;
-      logger().setContext(``);
       if (url + baseUrl == '/') return;
-      logger().log(
-        `[${reqN}-t:${t}]${method} ${url} ${baseUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} - ${data}`
+      console.log(
+        `[${reqN++}-t:${t}]${method} ${url} ${baseUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} - ${data}`
       );
     });
 
