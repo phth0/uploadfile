e119dffe81606d96c26c9e0b22551eca6fe82f51
chenxinhu <xingshi8000@gmail.com>
2021-02-24 16:31:59 +0800

    upload2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fstat } from 'fs';
 import { AppService } from './app.service';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
@@ -16,5 +18,15 @@ export class AppController {
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file) {
     console.log(file);
+  }
+
+  @Post('upload2')
+  async uploadFile2(@Body() message: any) {
+    console.log(message);
+    try {
+      fs.writeFileSync(process.env.UPLOAD + '/' + message.name, message.data);
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
