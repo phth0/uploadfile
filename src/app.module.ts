cc3d86da0fef02765318c59cdf88885145a01da3
chenxinhu <xingshi8000@gmail.com>
2021-02-24 14:45:10 +0800

    add log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReqlogMiddleware } from './reqlog.middleware';
 
 @Module({
   imports: [
@@ -14,4 +15,8 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(ReqlogMiddleware).forRoutes('*');
+  }
+}
