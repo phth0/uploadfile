b72354c15cf390c2cd7dd5b8a437465e0159270c
chenxinhu <xingshi8000@gmail.com>
2021-02-24 16:56:07 +0800

    set body size limit to 50M

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { MulterModule } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(parseInt(process.env.PORT) || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
